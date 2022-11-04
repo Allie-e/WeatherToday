b2cf386948d94a629a372c5d47de3e7f0c439896
wooyani77 <wooyani77@gmail.com>
2022-11-04 17:09:27 +0900

    feat: EndPoint에 forecastWeather path 추가 #1

--- WeatherToday/WeatherToday/Data/Network/EndPoint.swift
+++ WeatherToday/WeatherToday/Data/Network/EndPoint.swift
@@ -9,14 +9,26 @@ import Foundation
 
 enum EndPoint {
     private static let currentWeatherPath = "https://api.openweathermap.org/data/2.5/weather"
+    private static let forecastWeatherPath =  "https://api.openweathermap.org/data/3.0/onecall"
     private static let apiKey = "538f37ab841f4af5455c795ee0ad8e49"
     
     case currentWeather(Double, Double)
+    case forecastWeather(Double, Double)
     
     var url: URL? {
         switch self {
         case .currentWeather(let lat, let lon):
             var components = URLComponents(string: EndPoint.currentWeatherPath)
+            let latQuery = URLQueryItem(name: "lat", value: lat.description)
+            let lonQuery = URLQueryItem(name: "lon", value: lon.description)
+            let appidQuery = URLQueryItem(name: "appid", value: EndPoint.apiKey)
+            let unitsQuery = URLQueryItem(name: "units", value: "metric")
+            components?.queryItems = [latQuery, lonQuery, appidQuery, unitsQuery]
+            
+            return components?.url
+            
+        case .forecastWeather(let lat, let lon):
+            var components = URLComponents(string: EndPoint.forecastWeatherPath)
             let latQuery = URLQueryItem(name: "lat", value: lat.description)
             let lonQuery = URLQueryItem(name: "lon", value: lon.description)
             let appidQuery = URLQueryItem(name: "appid", value: EndPoint.apiKey)
