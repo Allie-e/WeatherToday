95e62be5396fe8c485f6486efbb268e233d3df44
wooyani77 <wooyani77@gmail.com>
2022-11-09 21:06:55 +0900

    feat: HourlyForecastCollectionViewCell 생성 및 구현 #7
    - Date extension으로 날짜형식 변환 메서드 추가

---
+++ WeatherToday/WeatherToday/Extension/Date+Extension.swift
@@ -0,0 +1,20 @@
+//
+//  Date+Extension.swift
+//  WeatherToday
+//
+//  Created by Allie on 2022/11/09.
+//
+
+import Foundation
+
+extension Date {
+    func toHourString() -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "a h시"
+        formatter.locale = Locale(identifier: "ko_KR")
+        formatter.amSymbol = "오전"
+        formatter.pmSymbol = "오후"
+        
+        return formatter.string(from: self)
+    }
+}
---
+++ WeatherToday/WeatherToday/Presentation/View/CurrentWeatherView/HourlyForecastCollectionViewCell.swift
@@ -0,0 +1,70 @@
+//
+//  HourlyForecastCollectionViewCell.swift
+//  WeatherToday
+//
+//  Created by Allie on 2022/11/09.
+//
+
+import UIKit
+import SnapKit
+
+final class HourlyForecastCollectionViewCell: UICollectionViewCell {
+    private let hourlyStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.alignment = .center
+        
+        return stackView
+    }()
+    
+    private let timeLabel: UILabel = {
+        let label = UILabel()
+        label.font = .preferredFont(forTextStyle: .caption1)
+        label.textColor = .white
+        
+        return label
+    }()
+    
+    private let weatherImageview: UIImageView = {
+        let imageView = UIImageView()
+        
+        return imageView
+    }()
+    
+    private let temperatureLabel: UILabel = {
+        let label = UILabel()
+        label.font = .preferredFont(forTextStyle: .caption1)
+        label.textColor = .white
+        
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubviews()
+        setupLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupHourlyForecastCell(with data: HourlyWeather) {
+        timeLabel.text = data.dt.toDate().toHourString()
+        weatherImageview.image = UIImage(named: String(data.weather[0].id))
+        temperatureLabel.text = data.temperature.toRoundedString()
+    }
+    
+    private func addSubviews() {
+        contentView.addSubview(hourlyStackView)
+        [timeLabel, weatherImageview, temperatureLabel].forEach { view in
+            hourlyStackView.addArrangedSubview(view)
+        }
+    }
+    
+    private func setupLayout() {
+        hourlyStackView.snp.makeConstraints { make in
+            make.edges.equalTo(contentView)
+        }
+    }
+}
